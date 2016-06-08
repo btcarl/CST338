577a27c5ee2fa6565ee2981620f2790775317849
Brian Carlston <Brian Carlston>
2016-06-07 23:13:53 -0400

    Git upload of soccer coach app

---
+++ SoccerCoach/app/src/main/java/com/btcarl/soccercoach/SoccerApplication.java
@@ -0,0 +1,10 @@
+package com.btcarl.soccercoach;
+
+import android.app.Application;
+
+/**
+ * Created by Brian on 10/23/2015.
+ */
+public class SoccerApplication extends Application {
+    public static final String path = "com.btcarl.soccercoach";
+}
